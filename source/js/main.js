af1e2242cd3e897cdf47fabf2de62d8de77d6a2f
CendresChaudes <romqaaa1337@gmail.com>
2023-06-17 13:38:53 +0300

    feat: добавляет стили и скрипт для блока sort
    * Убирает постфикс "icon" у иконок

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,5 @@
-import { initMapModule } from '../js/modules/map.js';
+import { initSort } from './modules/sort.js';
+import { initMapModule } from './modules/map.js';
 
+initSort();
 initMapModule();
