87d117c9b54086a1652cf099508e1b08a1492e2a
CendresChaudes <romqaaa1337@gmail.com>
2023-06-19 18:41:45 +0300

    feat: добавляет скрипт для мобильного меню

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,10 @@
+import { initMobileMenu } from './modules/mobile-menu.js';
 import { initRangeBar } from './modules/range.js';
 import { initSort } from './modules/sort.js';
 import { initPagination } from './modules/pagination.js';
 import { initMapModule } from './modules/map.js';
 
+initMobileMenu();
 initRangeBar();
 initSort();
 initPagination();
