968782cb799fe1e54980151fd217b6b9c3db0254
CendresChaudes <romqaaa1337@gmail.com>
2023-07-09 11:41:01 +0300

    Updates

---
+++ js/modules/slider.js
@@ -0,0 +1,1 @@
+const initSlider=()=>{new Swiper(".slider",{grabCursor:!0,slidesPerView:1,navigation:{prevEl:"#prev-hero-button",nextEl:"#next-hero-button",disabledClass:"slider__arrow-button--disabled"},pagination:{el:".slider__pagination",bulletElement:"button",bulletClass:"slider__pagination-button",bulletActiveClass:"slider__pagination-button--active",clickable:!0}})};export{initSlider};
